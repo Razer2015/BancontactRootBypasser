c3ba01e9ce24555b9eb95f3f9aa7a542677d0b9c
Razer2015 <razer138@gmail.com>
2017-06-20 13:06:16 +0300

    Fix for the latest Bancontact patch v2.3.5.894

--- app/src/main/java/fi/razerman/bancontactrootbypasser/RootDetectionBypass.java
+++ app/src/main/java/fi/razerman/bancontactrootbypasser/RootDetectionBypass.java
@@ -6,6 +6,8 @@ package fi.razerman.bancontactrootbypasser;
 
 
 import android.util.Log;
+
+import static de.robv.android.xposed.XposedBridge.log;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
@@ -26,23 +28,70 @@ public class RootDetectionBypass implements IXposedHookLoadPackage {
             Log.d(TAG, "Native methods bypassed!");
 
             // RootedDetector
-            findAndHookMethod("ʟ", lpparam.classLoader, "ˊ", returnConstant(false));                                    // Check 1
-            findAndHookMethod("ʟ", lpparam.classLoader, "ˋ", returnConstant(false));                                    // Check 2
-            findAndHookMethod("ʟ", lpparam.classLoader, "ˋ", android.content.Context.class, returnConstant(false));     // Check 3
-            findAndHookMethod("ʟ", lpparam.classLoader, "ˎ", returnConstant(false));                                    // Check 4
-            Log.d(TAG, "RootedDetector bypassed!");
+            try{
+                findAndHookMethod("ʟ", lpparam.classLoader, "ˊ", returnConstant(false));                                    // Check 1
+                findAndHookMethod("ʟ", lpparam.classLoader, "ˋ", returnConstant(false));                                    // Check 2
+                findAndHookMethod("ʟ", lpparam.classLoader, "ˋ", android.content.Context.class, returnConstant(false));     // Check 3
+                findAndHookMethod("ʟ", lpparam.classLoader, "ˎ", returnConstant(false));                                    // Check 4
+                Log.d(TAG, "Old RootedDetector bypassed!");
+            }
+            catch (Throwable e){
+                Log.d(TAG, "Old RootedDetector failed, trying a new one!");
+                try{
+                    findAndHookMethod("Ϊ", lpparam.classLoader, "ʻ", android.content.Context.class, returnConstant(false));     // Check 1
+                    findAndHookMethod("Ϊ", lpparam.classLoader, "ˋ", android.content.Context.class, returnConstant(false));     // Check 2
+                    findAndHookMethod("Ϊ", lpparam.classLoader, "ˎ", android.content.Context.class, returnConstant(false));     // Check 3
+                    findAndHookMethod("Ϊ", lpparam.classLoader, "ˏ", android.content.Context.class, returnConstant(false));     // Check 4
+                    findAndHookMethod("Ϊ", lpparam.classLoader, "ᐝ", android.content.Context.class, returnConstant(false));     // Check 5
+                    Log.d(TAG, "New RootedDetector bypassed!");
+                }
+                catch (Throwable ex){
+                    Log.d(TAG, "New RootedDetector failed!");
+
+                }
+            }
 
             // EmulatorDetector
-            findAndHookMethod("Ӏ", lpparam.classLoader, "ˊ", returnConstant(false));                                    // Check 1
-            findAndHookMethod("Ӏ", lpparam.classLoader, "ˋ", returnConstant(false));                                    // Check 2
-            findAndHookMethod("Ӏ", lpparam.classLoader, "ˋ", android.content.Context.class, returnConstant(false));     // Check 3
-            findAndHookMethod("Ӏ", lpparam.classLoader, "ˎ", returnConstant(false));                                    // Check 4
-            findAndHookMethod("Ӏ", lpparam.classLoader, "ˎ", android.content.Context.class, returnConstant(false));     // Check 5
-            Log.d(TAG, "EmulatorDetector bypassed!");
+            try{
+                findAndHookMethod("Ӏ", lpparam.classLoader, "ˊ", returnConstant(false));                                    // Check 1
+                findAndHookMethod("Ӏ", lpparam.classLoader, "ˋ", returnConstant(false));                                    // Check 2
+                findAndHookMethod("Ӏ", lpparam.classLoader, "ˋ", android.content.Context.class, returnConstant(false));     // Check 3
+                findAndHookMethod("Ӏ", lpparam.classLoader, "ˎ", returnConstant(false));                                    // Check 4
+                findAndHookMethod("Ӏ", lpparam.classLoader, "ˎ", android.content.Context.class, returnConstant(false));     // Check 5
+                Log.d(TAG, "Old EmulatorDetector bypassed!");
+            }
+            catch (Throwable e){
+                Log.d(TAG, "Old EmulatorDetector failed, trying a new one!");
+                try{
+                    findAndHookMethod("Ӏ", lpparam.classLoader, "ʻ", android.content.Context.class, returnConstant(false));     // Check 1
+                    findAndHookMethod("Ӏ", lpparam.classLoader, "ˋ", android.content.Context.class, returnConstant(false));     // Check 2
+                    findAndHookMethod("Ӏ", lpparam.classLoader, "ˎ", android.content.Context.class, returnConstant(false));     // Check 3
+                    findAndHookMethod("Ӏ", lpparam.classLoader, "ˏ", android.content.Context.class, returnConstant(false));     // Check 4
+                    findAndHookMethod("Ӏ", lpparam.classLoader, "ᐝ", android.content.Context.class, returnConstant(false));     // Check 5
+                    Log.d(TAG, "New EmulatorDetector bypassed!");
+                }
+                catch (Throwable ex){
+                    Log.d(TAG, "New EmulatorDetector failed!");
+
+                }
+            }
 
             // DebuggerDetector (USB Debugging)
-            findAndHookMethod("ﺫ", lpparam.classLoader, "ˋ", android.content.Context.class, returnConstant(false));     // Check 1
-            Log.d(TAG, "DebuggerDetector bypassed!");
+            try{
+                findAndHookMethod("ﺫ", lpparam.classLoader, "ˋ", android.content.Context.class, returnConstant(false));     // Check 1
+                Log.d(TAG, "Old DebuggerDetector bypassed!");
+            }
+            catch (Throwable e){
+                Log.d(TAG, "Old DebuggerDetector failed, trying a new one!");
+                try{
+                    findAndHookMethod("ĭ", lpparam.classLoader, "ˎ", android.content.Context.class, returnConstant(false));     // Check 1
+                    Log.d(TAG, "New DebuggerDetector bypassed!");
+                }
+                catch (Throwable ex){
+                    Log.d(TAG, "New DebuggerDetector failed!");
+
+                }
+            }
 
             // NOT WORKING, I don't know how to succeed in this because there is super.onResume(); call inside.
             // Visual USB Debugging pop up warning (different check) which is not preventing anything but just popping up.
